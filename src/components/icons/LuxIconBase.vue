9be07e527f1952e4af216f114a1ed55ff8bf8b70
Shaun Ellis <shaun@sdellis.com>
2023-12-20 17:13:10 -0500

    adds initial icons and creates sections for the styleguide

---
+++ src/components/icons/LuxIconBase.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="lux-icon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="width"
+      :height="height"
+      viewBox="0 0 24 24"
+      :aria-labelledby="iconName"
+      :aria-hidden="iconHide"
+      role="img"
+    >
+      <title v-if="iconName" :id="piconName" lang="en">{{ iconName }}</title>
+      <g :fill="iconColor">
+        <slot></slot>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+/**
+ * Icons are used to visually communicate core parts of the product and
+ * available actions. Please be aware that all elements must have closing tags (not "self-closing").
+ * To add additional icons, please consult [the instructions](/#/Adding%20Icons).
+ */
+export default {
+  name: "LuxIconBase",
+  status: "ready",
+  release: "1.0.0",
+  type: "Element",
+  props: {
+    /**
+     * The name of the icon to display. Also used for accessibility purposes.
+     */
+    iconName: {
+      required: false,
+      type: String,
+      default: "",
+    },
+    /**
+     * The width of the icon. For square icons use values of `11px, 13px,
+     * 16px, 24px, 36px, 48px, 64px` to follow visual heirachy set by
+     * typography.
+     */
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    /**
+     * The height of the icon. For square icons use values of `11px, 13px,
+     * 16px, 24px, 36px, 48px, 64px` to follow visual heirachy set by
+     * typography.
+     */
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+    /**
+     * The fill color of the SVG icon.
+     */
+    iconColor: {
+      type: String,
+      default: "currentColor",
+    },
+    /**
+     * Hides decorative icon from screen readers
+     */
+    iconHide: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/variables.css";
+.lux-icon {
+  display: inline-flex;
+  align-self: center;
+  margin: 0 var(--space-xx-small);
+
+  svg {
+    top: 0.125em;
+    position: relative;
+  }
+}
+</style>
+
+<docs>
+  ```jsx
+  <div>
+    <lux-icon-base width="30" height="30" icon-name="file">
+      <lux-icon-file></lux-icon-file>
+    </lux-icon-base>
+  </div>
+  ```
+</docs>
---
+++ src/components/icons/LuxIconFile.vue
@@ -0,0 +1,50 @@
+<template>
+  <g>
+    <g>
+      <path
+        d="M20.5,24h-17C3.2,24,3,23.8,3,23.5v-23C3,0.2,3.2,0,3.5,0h11c0.1,0,0.3,0.1,0.4,0.1l6,6C20.9,6.2,21,6.4,21,6.5v17
+        C21,23.8,20.8,24,20.5,24z M4,23h16V6.7L14.3,1H4V23z"
+      />
+    </g>
+    <g>
+      <path
+        d="M20.5,7h-6C14.2,7,14,6.8,14,6.5v-6C14,0.2,14.2,0,14.5,0S15,0.2,15,0.5V6h5.5C20.8,6,21,6.2,21,6.5S20.8,7,20.5,7z"
+      />
+    </g>
+  </g>
+</template>
+
+<script>
+/**
+ * Icons are used to visually communicate core parts of the product and
+ * available actions. Please be aware that all elements must have closing tags (not "self-closing").
+ * To add additional icons, please consult [the instructions](/#/Adding%20Icons).
+ */
+export default {
+  name: "LuxIconFile",
+  status: "ready",
+  release: "1.0.0",
+  type: "Element",
+}
+</script>
+
+<docs>
+  ```jsx
+  <div>
+    <!-- you can pass in a smaller `width` and `height` as props -->
+    <lux-icon-base width="12" height="12" icon-name="file">
+      <lux-icon-file></lux-icon-file>
+    </lux-icon-base>
+
+    <!-- or you can use the default, which is 18 -->
+    <lux-icon-base icon-name="file">
+      <lux-icon-file></lux-icon-file>
+    </lux-icon-base>
+
+    <!-- or make it a little bigger too, with colors :) -->
+    <lux-icon-base width="30" height="30" icon-name="file" icon-color="red">
+      <lux-icon-file></lux-icon-file>
+    </lux-icon-base>
+  </div>
+  ```
+</docs>
