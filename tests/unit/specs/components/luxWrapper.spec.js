35ac4d4953f7a6f60608901ecf44732bffce63e4
Jane Sandberg <js7389@princeton.edu>
2024-02-16 07:03:18 -0800

    Migrate the wrapper component from lux

---
+++ tests/unit/specs/components/luxWrapper.spec.js
@@ -0,0 +1,20 @@
+import { mount } from "@vue/test-utils"
+import LuxWrapper from "@/components/LuxWrapper.vue"
+import { nextTick } from "vue"
+
+describe("LuxWrapper.vue", () => {
+  let wrapper
+
+  it("can accept an arbitrary number of pixels in the maxWidth prop", () => {
+    wrapper = mount(LuxWrapper, {
+      props: {
+        maxWidth: "123",
+      },
+    })
+    expect(wrapper.find("div").attributes("style")).toEqual("max-width: 123px;")
+  })
+
+  it("has the expected html structure", () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
