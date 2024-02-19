11628b271049b193c137dabe2acf3d6726caa361
Jane Sandberg <js7389@princeton.edu>
2024-02-19 09:38:02 -0800

    Remove main.js
    It is not being called when running the styleguide or when building the bundle

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue/dist/vue.esm-bundler"
-import App from "./App.vue"
-import "./registerServiceWorker"
-import store from "./store"
-
-createApp(App).use(store).mount("#app")
-
-import LuxCard from "./components/LuxCard.vue"
