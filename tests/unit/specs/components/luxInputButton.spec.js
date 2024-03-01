6a18ff7a4bc8beb4d6202d168e28700ef3ba1647
Max Kadel <mkadel@princeton.edu>
2024-03-01 16:33:04 -0500

    Add test for icon in LuxInputButton
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- tests/unit/specs/components/luxInputButton.spec.js
+++ tests/unit/specs/components/luxInputButton.spec.js
@@ -1,5 +1,6 @@
 import { mount } from "@vue/test-utils"
 import LuxInputButton from "@/components/LuxInputButton.vue"
+import LuxIconBase from "@/components/icons/LuxIconBase.vue"
 
 describe("LuxInputButton.vue", () => {
   let wrapper
@@ -9,14 +10,42 @@ describe("LuxInputButton.vue", () => {
       wrapper = mount(LuxInputButton, {
         props: {
           size: "small",
+          variation: "outline",
+        },
+        global: {
+          components: { LuxIconBase },
         },
       })
       expect(wrapper.get("button").classes()).toContain("small")
     })
 
     it("should have a default size", () => {
-      wrapper = mount(LuxInputButton, {})
+      wrapper = mount(LuxInputButton, {
+        props: {
+          variation: "outline",
+        },
+        global: {
+          components: { LuxIconBase },
+        },
+      })
       expect(wrapper.get("button").classes()).toContain("medium")
     })
+  })
+
+  describe("Icon", () => {
+    // Note: We need to add an icon prop to this - it does not actually show an icon
+    // Also need to add a better assertion that indicates that a user actually sees an icon on the screen
+    it("displays an icon", () => {
+      wrapper = mount(LuxInputButton, {
+        props: {
+          variation: "icon",
+        },
+        global: {
+          components: { LuxIconBase },
+        },
+      })
+      expect(wrapper.get("button").classes()).toContain("icon")
+    })
+    it.todo("does not display an icon")
   })
 })
