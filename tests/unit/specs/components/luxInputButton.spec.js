00824e522a3c0eeeb416c18acd864ee169e6838d
Max Kadel <mkadel@princeton.edu>
2024-03-01 15:30:19 -0500

    Start structure for LuxInputButton tests
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

---
+++ tests/unit/specs/components/luxInputButton.spec.js
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils"
+import LuxInputButton from "@/components/LuxInputButton.vue"
+
+describe("LuxInputButton.vue", () => {
+  let wrapper
+
+  it("should render different sizes", () => {})
+})
