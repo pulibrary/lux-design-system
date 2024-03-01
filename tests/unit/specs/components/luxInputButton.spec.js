88c84965de62403d4faf99ccc8b2fc8e4c9ff544
Beck Davis <bd4538@princeton.edu>
2024-03-01 16:00:46 -0500

    Add two tests about button sizes
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

--- tests/unit/specs/components/luxInputButton.spec.js
+++ tests/unit/specs/components/luxInputButton.spec.js
@@ -4,5 +4,19 @@ import LuxInputButton from "@/components/LuxInputButton.vue"
 describe("LuxInputButton.vue", () => {
   let wrapper
 
-  it("should render different sizes", () => {})
+  describe("ButtonSize", () => {
+    it("should render different sizes", () => {
+      wrapper = mount(LuxInputButton, {
+        props: {
+          size: "small",
+        },
+      })
+      expect(wrapper.get("button").classes()).toContain("small")
+    })
+
+    it("should have a default size", () => {
+      wrapper = mount(LuxInputButton, {})
+      expect(wrapper.get("button").classes()).toContain("medium")
+    })
+  })
 })
