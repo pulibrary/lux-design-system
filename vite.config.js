786ca23ef4690d044f4654d7987edffb658c057e
Eliot Jordan <eliotj@princeton.edu>
2024-04-11 12:05:10 -0500

    Copy scss to dist and export

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vue from "@vitejs/plugin-vue"
 import { resolve } from "path"
 import { defineConfig } from "vite"
 import { splitVendorChunkPlugin } from "vite"
+import copy from "rollup-plugin-copy"
 
 const vueDocsPlugin = {
   name: "vue-docs",
@@ -12,7 +13,15 @@ const vueDocsPlugin = {
 }
 
 export default defineConfig({
-  plugins: [vue(), splitVendorChunkPlugin(), vueDocsPlugin],
+  plugins: [
+    vue(),
+    splitVendorChunkPlugin(),
+    vueDocsPlugin,
+    copy({
+      targets: [{ src: "src/assets/styles/", dest: "dist/" }],
+      hook: "writeBundle",
+    }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
