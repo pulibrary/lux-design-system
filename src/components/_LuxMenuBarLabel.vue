b0d57265a0c6e14b963afdb5743202957b3305a6
Jane Sandberg <js7389@princeton.edu>
2024-08-07 10:38:24 -0700

    [LuxMenuBar] Add new unsafe_name property to the menu item object
    `unsafe_name` works like the existing name property, except that if it contains some
    arbitrary HTML, it does not escape it.  The immediate use case for this behavior
    is the "Bookmarks (4)" menu item in the catalog, which has a span with a specific class
    around the number that allows Blacklight's javascript to update it as the user adds/removes
    bookmarks.
    
    This has security implications, since `unsafe_name` bypasses Vue's cross-site scripting
    protections.  I've added documentation about this (as well as other properties on this
    object that did not yet have this documentation).
    
    Interestingly, the `buttons` version of LuxMenuBar was already bypassing cross-site
    scripting protections for the `name` property.  In the interest of consistency and safe
    defaults, this commit make it match the behavior of the others: you can add arbitrary
    HTML with `unsafe_name`, not with `name`.
    
    Helps with pulibrary/orangelight#4088

---
+++ src/components/_LuxMenuBarLabel.vue
@@ -0,0 +1,30 @@
+<template>
+  <span v-if="item.unsafe_name" v-html="item.unsafe_name"></span
+  ><template v-else>{{ item.name }}</template>
+</template>
+<script setup>
+defineProps({
+  item: Object,
+})
+</script>
+<docs>
+  ```jsx
+    <ul>
+    <li><lux-menu-bar-label :item="
+    {name: 'Logout', href: '/logout'}
+    "/></li>
+    <li><lux-menu-bar-label :item="
+    {unsafe_name: 'Bookmarks <strong>(1 / 3)</strong>', href: '/logout'}
+    "/></li>
+  ```
+  Security considerations:
+  <ul>
+    <li>You can add any arbitrary HTML to the <code>unsafe_name</code> property,
+      and it will be rendered for the user.  If you don't need to add arbitrary
+      HTML, use the <code>name</code> property instead.  Don't bind the
+      <code>unsafe_name</code> property to any user-provided value, since it does
+      not have Cross-Site Scripting protections (<code>name</code> does have these
+      protections).
+    </li>
+  </ul> 
+</docs>
