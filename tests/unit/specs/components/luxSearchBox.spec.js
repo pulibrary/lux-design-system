937f411bab7f8428452d6e4c3c175403bd5841e2
Jane Sandberg <js7389@princeton.edu>
2025-06-16 10:52:32 -0700

    [LuxSearchBox] implement v-model

--- tests/unit/specs/components/luxSearchBox.spec.js
+++ tests/unit/specs/components/luxSearchBox.spec.js
@@ -6,4 +6,16 @@ describe("LuxSearchBox", () => {
     const wrapper = mount(LuxSearchBox)
     expect(wrapper.find('button[type="submit"]').exists()).toBe(true)
   })
+  it("implements v-model", async () => {
+    const wrapper = mount(LuxSearchBox, {
+      props: {
+        modelValue: "squid",
+        "onUpdate:modelValue": e => wrapper.setProps({ modelValue: e }),
+      },
+    })
+
+    expect(wrapper.props("modelValue")).toBe("squid")
+    await wrapper.find("input").setValue("nautilus")
+    expect(wrapper.props("modelValue")).toBe("nautilus")
+  })
 })
