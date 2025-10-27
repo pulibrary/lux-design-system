1a09643b431082aea1b7e6b8e7d4c5e71d415e07
Carolyn Cole <cac9@princeton.edu>
2025-10-27 13:56:37 -0400

    Started with the existing component Want to recognize that there is an input based on an even in the base component and do the stuff to copy it below

--- src/components/LuxMultiSelect.vue
+++ src/components/LuxMultiSelect.vue
@@ -1,3 +1,22 @@
 <template>
   <div>hello!</div>
+  <lux-autocomplete-input
+    label="Fruit"
+    default-value="Banana"
+    :items="[
+      'Apple',
+      'Banana',
+      'Banana split',
+      'Orange',
+      'Mango',
+      'Pear',
+      'Peach',
+      'Grape',
+      'Tangerine',
+      'Pineapple',
+    ]"
+  />
 </template>
+<script setup>
+import LuxAutocompleteInput from "./LuxAutocompleteInput.vue"
+</script>
