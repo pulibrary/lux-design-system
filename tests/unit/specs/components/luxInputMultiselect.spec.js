8c45a3bcfcec3e576317b99890c72ac9d382758b
Jane Sandberg <js7389@princeton.edu>
2025-10-28 12:12:46 -0700

    Breaking change: Replace LuxInputMultiselect with our new implementation

--- tests/unit/specs/components/luxInputMultiselect.spec.js
+++ tests/unit/specs/components/luxInputMultiselect.spec.js
@@ -1,62 +1,23 @@
-import { createLocalVue, mount } from "@vue/test-utils"
+import { mount } from "@vue/test-utils"
 import LuxInputMultiselect from "@/components/LuxInputMultiselect.vue"
+import { nextTick } from "vue"
 
-describe("LuxInputMultiselect.vue", () => {
-  let wrapper
+describe("MultiSelect.vue", () => {
+  it("adds selected items to the selected area", async () => {
+    const wrapper = mount(LuxInputMultiselect)
+    const input = wrapper.find("#displayInput")
+    input.trigger("focus")
+    // Enter the first few letters of mango
+    input.setValue("mang")
+    // Wait for Vue to re-render the DOM
+    await nextTick()
 
-  beforeEach(() => {
-    wrapper = mount(LuxInputMultiselect, {
-      stubs: ["wrapper", "input-text"],
-      propsData: {
-        id: "test_id",
-        options: [
-          { name: "Vue.js", code: "vu" },
-          { name: "Javascript", code: "js" },
-          { name: "Open Source", code: "os" },
-        ],
-        value: [{ name: "Javascript", code: "js" }],
-      },
-    })
-  })
+    const mangoItem = wrapper.find(".lux-autocomplete-result")
+    expect(mangoItem.text()).toEqual("Mango")
+    mangoItem.trigger("mousedown")
 
-  it("should accept an optional label", async () => {
-    expect(wrapper.find("label").exists()).toBe(false)
-    await wrapper.setProps({ inputLabel: "Multiple choice:" })
-    expect(wrapper.find("label").exists()).toBe(true)
-    expect(wrapper.find("label").text()).toBe("Multiple choice:")
-  })
+    await nextTick()
 
-  it("should connect the label to the field using the id", async () => {
-    await wrapper.setProps({ inputLabel: "Multiple choice:" })
-    expect(wrapper.find("label").attributes("for")).toBe("test_id")
-    expect(wrapper.find("input").attributes("id")).toBe("test_id")
-  })
-
-  it("should have a list of options", () => {
-    const allOptions = wrapper.findAll("li")
-
-    expect(allOptions.at(0).text()).toBe("Vue.js")
-    expect(allOptions.at(1).text()).toBe("Javascript")
-    expect(allOptions.at(2).text()).toBe("Open Source")
-  })
-
-  it("should have selected options", () => {
-    const selected = wrapper.findAll(".multiselect__tag")
-    expect(selected.length).toBe(1)
-    expect(selected.at(0).text()).toBe("Javascript")
-  })
-
-  it("should have values associated with the names", () => {
-    const selected_hash = wrapper.find("code")
-    expect(selected_hash.text().includes('code": "js"')).toBe(true)
-  })
-
-  it("should have an optional placeholder with a default", async () => {
-    var placeholder = wrapper.find("input").attributes("placeholder")
-
-    expect(placeholder).toBe("Search or add a tag")
-    await wrapper.setProps({ placeholder: "Search or add an accessibility need" })
-    var placeholder = wrapper.find("input").attributes("placeholder")
-    expect(placeholder).toBe("Search or add an accessibility need")
+    expect(wrapper.find("#selected").text()).toContain("Mango")
   })
 })
