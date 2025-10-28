777e6014414723f828cda31a807ddd1c587cff6e
Jane Sandberg <js7389@princeton.edu>
2025-10-28 14:19:07 -0700

    Add placeholder

--- src/components/LuxInputMultiselect.vue
+++ src/components/LuxInputMultiselect.vue
@@ -1,7 +1,16 @@
 <template>
-  <lux-autocomplete-input :items="props.items" @selected="addSelected($event)" ref="autocomplete" />
+  <lux-autocomplete-input
+    :items="props.items"
+    @selected="addSelected($event)"
+    ref="autocomplete"
+    :placeholder="placeholder"
+  />
   <ul class="selected-items">
     <li v-for="item in selectedItems" :key="item" class="selected-item">
+      <!--
+        @slot item -- used to adjust the style and format of the items you have selected
+          @binding {object} itemProps an individual item that you would like to style
+      -->
       <slot name="item" :itemProps="item">{{ item.label }}</slot>
       <lux-input-button
         @button-clicked="removeItem(item)"
@@ -14,6 +23,11 @@
       </lux-input-button>
     </li>
   </ul>
+  <!--
+  @slot hiddenInput -- useful for embedding this component into an HTML form
+      @binding {array} selectedItems an array of items (objects) that the user has selected
+  -->
+  <slot name="hiddenInput" :selectedItems="selectedItems"></slot>
 </template>
 <script setup>
 import LuxAutocompleteInput from "./LuxAutocompleteInput.vue"
@@ -21,7 +35,7 @@ import LuxInputButton from "./LuxInputButton.vue"
 import { ref, useTemplateRef } from "vue"
 
 const selectedItems = ref([])
-const props = defineProps(["items"])
+const props = defineProps(["items", "placeholder"])
 const autocompleteRef = useTemplateRef("autocomplete")
 
 function addSelected(id) {
@@ -49,7 +63,6 @@ function removeItem(item) {
 
   .selected-item {
     display: flex;
-    /* height: 2.25rem; */
     padding: 0.25rem 0.75rem;
     align-items: center;
     justify-content: space-between;
@@ -68,7 +81,7 @@ function removeItem(item) {
           { id: 2, label: 'Banana' },
           { id: 3, label: 'Banana split' },
           { id: 4, label: 'Mango' },
-        ]"/>
+        ]" placeholder="Please choose a fruit" />
     <p>If you have a specific way you'd like to display the items, you can pass it as a template into the item slot:</p>
     <lux-input-multiselect :items="[
           { id: 1, label: 'Apple' },
