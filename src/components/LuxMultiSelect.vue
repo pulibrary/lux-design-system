696ad42aa505347e2f5b39bfbaa0bfb99c96a2e2
Carolyn Cole <cac9@princeton.edu>
2025-10-28 13:29:28 -0400

    adding the new event to our component

--- src/components/LuxMultiSelect.vue
+++ src/components/LuxMultiSelect.vue
@@ -15,8 +15,20 @@
       'Tangerine',
       'Pineapple',
     ]"
+    @selected="addSelected(id)"
   />
+  <ul id="selected">
+    <li v-for="item in selectedItems" :key="item">
+      {{ item }}
+    </li>
+  </ul>
 </template>
 <script setup>
 import LuxAutocompleteInput from "./LuxAutocompleteInput.vue"
+import { ref } from "vue"
+const selectedItems = ref([])
+
+function addSelected(id) {
+  selectedItems.value.push(id)
+}
 </script>
