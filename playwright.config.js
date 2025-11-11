8dd556a2285df776c76e74ad29d44a57854babb1
Carolyn Cole <cac9@princeton.edu>
2025-11-11 15:52:42 -0500

    Adding a failing test for enter submitting the form

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:5002",
+    url: "http://127.0.0.1:5002",
     reuseExistingServer: !process.env.CI,
   },
 })
