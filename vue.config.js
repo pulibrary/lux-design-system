95d7326e905fb0b7fc83936c63fe0901c9b436fe
Christina Chortaria <actspatial@gmail.com>
2026-09-02 13:33:19 -0400

    Use relative path for scss file in LuxAlert.vue Remove unneeded plugin from .vitepress/config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require("@vue/cli-service")
-module.exports = defineConfig({
-  transpileDependencies: true,
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+export default defineConfig({
+  plugins: [vue()],
 })
 
 // Helpful for sass loader config
