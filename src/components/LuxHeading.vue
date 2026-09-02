67de23c59f09c1372a630d4247af1536ba450127
Christina Chortaria <actspatial@gmail.com>
2026-09-02 18:22:56 -0400

    Add again the docsgen config set different output directory so that when we run it and test it it will add files under docs/. In index.js import LuxAlert, LuxDialog, LuxBadge Remove docs section from the relevant components that are loaded through the imported ones in index.js. vitepress does not support loading a component that has docs inside unless we use a specific plugin.
    `npm run docs:build`
    `npm run docs:dev`
    Go to: http://localhost:5173/src/components/LuxAlert

--- src/components/LuxHeading.vue
+++ src/components/LuxHeading.vue
@@ -58,8 +58,8 @@ const props = defineProps({
 </script>
 
 <style lang="scss">
-@use "/src/assets/styles/mixins.scss" as *;
-@use "/src/assets/styles/spacing.scss" as *;
+@use "../assets/styles/mixins.scss" as *;
+@use "../assets/styles/spacing.scss" as *;
 
 .lux-heading {
   @include reset;
@@ -105,35 +105,3 @@ const props = defineProps({
   font-weight: var(--font-weight-regular);
 }
 </style>
-
-<docs>
-  ```jsx
-  <div>
-    <lux-heading level="h1" hidden>The quick brown fox</lux-heading>
-    <lux-heading level="h2" size="h1">The quick brown fox</lux-heading>
-    <lux-heading level="h3" size="h3">The quick brown fox</lux-heading>
-    <lux-heading level="h4" size="h5">The quick brown fox</lux-heading>
-  </div>
-  ```
-
-  ```jsx noeditor
-  <div class="dos-n-donts">
-    <div class="do">
-      <div class="do-dont-example">
-        <lux-heading level="h1">This is an h1</lux-heading>
-        <lux-heading level="h2" size="h2">This is an h2</lux-heading>
-      </div>
-      <p>Nest headings appropriately by level. You can pick the desired font size with the size prop.</p>
-    </div>
-
-    <div class="dont">
-      <div class="do-dont-example">
-        <lux-heading level="h2" size="h2">This is an h2</lux-heading>
-        <lux-heading level="h1">This is an h1</lux-heading>
-      </div>
-      <p>If you nest headings out of order or skip levels, the lack of organization of content on the page will confuse users.</p>
-    </div>
-  </div>
-  ```
-
-</docs>
