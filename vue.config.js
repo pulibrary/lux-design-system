933c7bcc31936b45e59a7c99a9152d8c6f00abd9
Christina Chortaria <actspatial@gmail.com>
2026-09-17 15:52:53 -0400

    revert changes of vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-
-export default defineConfig({
-  plugins: [vue()],
+const { defineConfig } = require("@vue/cli-service")
+module.exports = defineConfig({
+  transpileDependencies: true,
 })
 
 // Helpful for sass loader config
