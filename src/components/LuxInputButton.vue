26671ffc61e3d0f1ecb7f0e5001d9e8b9d75d6fa
Christina Chortaria <actspatial@gmail.com>
2026-09-22 15:27:49 -0400

    [LuxIconBase] Remove unused imports toRef and wach
    lux_icon_base.vue is being parsed as a normal command line argument it is caused when
    running npm run lint --fix from lint-staged.config.js
    
    Update npm run lint -- --fix to include the -- See https://docs.npmjs.com/cli/v11/commands/npm-run
    ```
    Any positional arguments are passed to the specified script. Use -- to pass --prefixed flags and options which would otherwise be parsed by npm.
    ```

--- src/components/LuxInputButton.vue
+++ src/components/LuxInputButton.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script setup>
-import { computed, defineOptions, onUpdated, toRef, useTemplateRef, watch } from "vue"
+import { computed, defineOptions, onUpdated, useTemplateRef } from "vue"
 import LuxIconBase from "./icons/LuxIconBase.vue"
 
 /**
